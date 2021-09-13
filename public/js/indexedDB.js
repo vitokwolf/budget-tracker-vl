9891d2fd348c8e57027fe82a57c904486c78d0f6
Victor Lupascu <sinajeen@gmail.com>
2021-09-13 13:05:32 -0500

    fix async errors

--- public/js/indexedDB.js
+++ public/js/indexedDB.js
@@ -2,56 +2,65 @@ let db
 const request = indexedDB.open('budget', 1)
 
 //in the event of database version changes
-request.onupgradeneeded = function (e) {
-  const newDb = e.target.result
-  newDb.createObjectStore('pending', { autoIncrement: true })
+request.onupgradeneeded = (e) => {
+  const db = e.target.result
+  db.createObjectStore('pending', { autoIncrement: true })
 }
 
 //upon a successful creation
-request.onsuccess = function (e) {
+request.onsuccess = (e) => {
   db = e.target.result
   //   check if online, and if true, update database
-  if (navigator.online) {
+  if (navigator.onLine) {
     offlineData()
   }
 }
 
 // error handling
-request.onerror = function (e) {
-  console.log(e.target.errorCode)
-}
+request.onerror = (e) => console.log(e.target.errorCode)
 
 // function to store data when offline
-const saveRecord = function (record) {
+const saveRecord = (record) => {
   const transaction = db.transaction(['pending'], 'readwrite')
   const store = transaction.objectStore('pending')
   store.add(record)
 }
 
-const offlineData = function () {
+const offlineData = () => {
   const transaction = db.transaction(['pending'], 'readwrite')
   const store = transaction.objectStore('pending')
   const allRecords = store.getAll()
 
-  allRecords.onsuccess = async function () {
+  allRecords.onsuccess = () => {
     // if any data stored in indexDB, update database
     if (allRecords.result.length > 0) {
-      const post = await fetch('/api/transaction/bulk', {
+      fetch('/api/transaction/bulk', {
         method: 'POST',
-        body: JSON.stringify(getAll.result),
+        body: JSON.stringify(allRecords.result),
         headers: {
           Accept: 'application/json, text/plain, */*',
           'Content-Type': 'application/json',
         },
       })
-      const json = await post.json()
-      if (json.message) {
-        throw new Error(json)
-      }
-      const transaction = db.transaction(['pending'], 'readwrite')
-      const store = transaction.objectStore('pending')
-      store.clear()
-      alert('Offline activity has been uploaded to your database')
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse)
+          }
+          //open one more transaction
+          const transaction = db.transaction(['pending'], 'readwrite')
+
+          //access the pending object store
+          const store = transaction.objectStore('pending')
+
+          //clear all items in store
+          store.clear()
+
+          alert('All pending transactions have been submitted!')
+        })
+        .catch((err) => {
+          console.log(err)
+        })
     }
   }
 }
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,4 @@
+//define gloabl constants
 const APP_PREFIX = 'Budget-'
 const VERSION = 'version_01'
 const CACHE_NAME = APP_PREFIX + VERSION
@@ -11,10 +12,10 @@ const FILES_TO_CACHE = [
   './manifest.json',
 ]
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install', function (e) {
   //perform install steps
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
+    caches.open(CACHE_NAME).then(function (cache) {
       console.log('Opened cache')
       return cache.addAll(FILES_TO_CACHE)
     }),
@@ -22,14 +23,16 @@ self.addEventListener('install', (e) => {
 })
 
 //activate and instructions to manage cache
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', function (e) {
   e.waitUntil(
-    caches.keys().then((keyList) => {
-      let cacheKeeplist = keyList.filter((key) => key.indexOf(APP_PREFIX))
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
       cacheKeeplist.push(CACHE_NAME)
 
       return Promise.all(
-        keyList.map((key, i) => {
+        keyList.map(function (key, i) {
           if (cacheKeeplist.indexOf(key) === -1) {
             console.log('deleting cache : ' + keyList[i])
             return caches.delete(keyList[i])
@@ -41,11 +44,22 @@ self.addEventListener('activate', (e) => {
 })
 
 //intercept fetch requests
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', function (e) {
   console.log('fetch request : ' + e.request.url)
   e.respondWith(
-    caches
-      .match(e.request)
-      .then((request) => (request ? request : fetch(e.request))),
+    caches.match(e.request).then(function (request) {
+      // if cache is available, respond with cache
+      if (request) {
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {
+        // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
+
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    }),
   )
 })
