d4e4867e8c424826f4e95bb1a847c537a26e6540
Victor Lupascu <sinajeen@gmail.com>
2021-09-13 12:33:09 -0500

    add upload offline data to db when online

--- public/js/indexedDB.js
+++ public/js/indexedDB.js
@@ -12,7 +12,7 @@ request.onsuccess = function (e) {
   db = e.target.result
   //   check if online, and if true, update database
   if (navigator.online) {
-    uploadPending()
+    offlineData()
   }
 }
 
@@ -27,3 +27,33 @@ const saveRecord = function (record) {
   const store = transaction.objectStore('pending')
   store.add(record)
 }
+
+const offlineData = function () {
+  const transaction = db.transaction(['pending'], 'readwrite')
+  const store = transaction.objectStore('pending')
+  const allRecords = store.getAll()
+
+  allRecords.onsuccess = async function () {
+    // if any data stored in indexDB, update database
+    if (allRecords.result.length > 0) {
+      const post = await fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+      const json = await post.json()
+      if (json.message) {
+        throw new Error(json)
+      }
+      const transaction = db.transaction(['pending'], 'readwrite')
+      const store = transaction.objectStore('pending')
+      store.clear()
+      alert('Offline activity has been uploaded to your database')
+    }
+  }
+}
+
+window.addEventListener('online', offlineData)
